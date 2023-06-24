7fcd98a27895500ea7c97b25a870160fec6675ea
pawanlaksahan <pawanlaksahan10@gmail.com>
2023-06-25 02:06:21 +0530

    Creating product page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import './Product.scss'
+import './Product.scss';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import CompareIcon from '@mui/icons-material/Compare';
+
 
 export const Product = () => {
 
@@ -9,6 +15,7 @@ export const Product = () => {
   ];
 
   const[Image,setImage]=useState(0);
+  const[value,setValue]=useState(0);
 
   return (
     <div className="product">
@@ -26,22 +33,89 @@ export const Product = () => {
           <h2>Wine</h2>
         </div>
         <div className="price">
-          Rs.2500
+          <h2>Rs.2500</h2>
         </div>
         <div className="para">
-          <p>tretrtrltretrllrktrket;rrtretretretrt
-            tretretrtretrtrtrtrtretretrrtretrtrt
-            rtretretreetretretretretetrtretete
+          <p>Wine is an alcoholic drink typically made from fermented grapes.
+             Yeast consumes the sugar in the grapes and converts it to ethanol and carbon dioxide, releasing heat in the process.
+             Different varieties of grapes and strains of yeasts are major factors in different styles of wine.
+             These differences result from the complex interactions between the biochemical development of the grape, the reactions involved in fermentation, the grape's growing environment (terroir), and the wine production process.
+             Many countries enact legal appellations intended to define styles and qualities of wine. These typically restrict the geographical origin and permitted varieties of grapes, as well as other aspects of wine production.
+             Wines can be made by fermentation of other fruit crops such as plum, cherry, pomegranate, blueberry, currant and elderberry.
           </p>
         </div>
         <div className="amount">
-          <div className="decrease"></div>
-          <div className="number"></div>
-          <div className="increase"></div>
+          <button className="decrease_increase" onClick={(e)=>{setValue(value===1?1:prev=>prev-1)}}>
+          <RemoveIcon />
+          </button>
+          <div className="number">{value}</div>
+          <button className="decrease_increase" onClick={(e)=>setValue(prev=>prev+1)}>
+            <AddIcon />
+          </button>
         </div>
-        <div className="addToCart">
-          
+        <button className="addToCart">
+          <div className="icon">
+            <ShoppingCartIcon/>
+          </div>
+          <div className="text">
+            Add To CArt
+          </div>
+        </button>
+
+        <div className="actions">
+        <div className="wishlist_compare">
+          <div className="icon">
+            <FavoriteBorderIcon/>
+          </div>
+          <div className="text">
+            Add to wishlist
+          </div>
         </div>
+
+        <div className="wishlist_compare">
+          <div className="icon">
+            <CompareIcon/>
+          </div>
+          <div className="text">
+            add to compare
+          </div>
+        </div>
+
+        </div>
+        
+       
+       <div className="text">
+          Vendor : Polo
+        </div>
+        <div className="text">
+          Product Type : Red Wine
+        </div>
+        <div className="text">
+          Tag : Alcohol, Natural
+        </div>
+       
+        
+
+        <hr></hr>
+
+        <div className="details">
+
+        <div className="description">
+          Description
+        </div>
+        <hr style={{marginTop:'10px',width:'200px',marginLeft:'0px'}}/> 
+        <div className="description">
+          Additional Information
+        </div>
+        <hr style={{marginTop:'10px',width:'200px',marginLeft:'0px'}}/> 
+          <div className="description">
+            FAq
+          </div>
+
+        </div>
+       
+
+
 
       </div>
     </div>
